88a736dbd5ea1d369fec5c1e717805e732751bc2
Rdm-jony <jonydascse@gmail.com>
2024-10-30 00:24:00 +0600

    set up route

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './Common/Navbar'
+import { Outlet } from 'react-router-dom'
+import Footer from './Common/Footer'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <Navbar></Navbar>
+     <Outlet></Outlet>
+     <Footer></Footer>
+    </>
+  )
+}
+
+export default App
